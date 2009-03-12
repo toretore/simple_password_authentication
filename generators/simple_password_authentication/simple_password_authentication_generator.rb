77a9950b631ed02df1db4e1a22e7256dbc6d72e5
Tore Darell <toredarell@gmail.com>
2009-03-12 16:08:45 +0100

    Update generator

--- generators/simple_password_authentication/simple_password_authentication_generator.rb
+++ generators/simple_password_authentication/simple_password_authentication_generator.rb
@@ -1,7 +1,7 @@
 class SimplePasswordAuthenticationGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      m.migration_template "migration:migration.rb", "db/migrate", :assigns => simple_auth_assigns,
+      m.migration_template "migration:migration.rb", "db/migrate", :assigns => password_auth_assigns,
         :migration_file_name => "add_password_authentication_fields_to_#{custom_file_name}" if options[:migration]
     end
   end
@@ -14,16 +14,16 @@ private
     custom_name 
   end
 
-  def simple_auth_assigns
+  def password_auth_assigns
     returning({}) do |assigns|
       assigns[:migration_action] = "add"
       assigns[:class_name] = "add_password_authentication_fields_to_#{custom_file_name}"
       assigns[:table_name] = "users"
-      assigns[:attributes] = simple_auth_attributes
+      assigns[:attributes] = password_auth_attributes
     end
   end
 
-  def simple_auth_attributes
+  def password_auth_attributes
     returning attributes.dup do |attributes|
       {"username" => "string", "hashed_password" => "string", "salt" => "string"}.each do |name, type|
         attributes << Rails::Generator::GeneratedAttribute.new(name, type) unless attributes.any?{|a| a.name == name }
