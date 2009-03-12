449ad26efc5549550986d2cdd47f6f27a1ebadbf
Tore Darell <toredarell@gmail.com>
2009-03-12 16:08:56 +0100

    Add i18n load path to the beginning to allow values to be overridden

--- lib/simple_password_authentication.rb
+++ lib/simple_password_authentication.rb
@@ -7,4 +7,4 @@ SimpleAuthentication::ModelMethods::User.send(:include, SimplePasswordAuthentica
 SimpleAuthentication::ModelMethods::User::ClassMethods.send(:include, SimplePasswordAuthentication::ModelMethods::User::ClassMethods)
 #User.send(:include, SimplePasswordAuthentication::ModelMethods::User::Behavior)
 SimpleAuthentication::ControllerMethods::Logins.send(:include, SimplePasswordAuthentication::ControllerMethods::Logins)
-I18n.load_path << File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_password_authentication.yml')
+I18n.load_path.unshift(File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_password_authentication.yml'))
